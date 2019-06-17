c1ba080b785212ad9c91b3626d1eecaa153c3ece
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-06-17 16:13:01 +0100

    Update dogyearcalculator.js
    tidied up

--- dogyearcalculator.js
+++ dogyearcalculator.js
@@ -1,15 +1,15 @@
-var myAge = 23;
-//age of user - input your age here. You don't need to do anything else!
-let earlyYears = 2;
+  var myAge = 23;
+//age of user - input your age here. You don't need to do anything else! 23 is the placeholder as I'm 23 years old
+  let earlyYears = 2;
 //this value will change due to the calculation 
-earlyYears *= 10.5;
-let laterYears = myAge - 2;
-laterYears *= 4;
+  earlyYears *= 10.5;
+  let laterYears = myAge - 2;
+  laterYears *= 4;
 //calcs later years
-console.log(laterYears);
-console.log(earlyYears);
-var myAgeInDogYears = laterYears + earlyYears;
+  console.log(laterYears);
+  console.log(earlyYears);
+  var myAgeInDogYears = laterYears + earlyYears;
 //calculates dog years age
-var myName = 'Cameron'.toLowerCase();
+  var myName = 'Cameron'.toLowerCase();
 //returns string of lowercase 
-console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
+  console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
